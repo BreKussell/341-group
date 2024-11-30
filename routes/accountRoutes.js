e93b17204b26cdfcb5a5030802583935c1812646
Breanna Russell <brekayussell@gmail.com>
2024-11-30 16:14:23 -0700

    Register user function added, and register routes added

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,9 +1,12 @@
+//Routes***************************************************************
 const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
 
 router.get('/login', (req, res) => res.render('login'));
 router.post('/login', accountController.login);
+router.get('/register', (req, res) => res.render('register'));
+router.post('/register', accountController.register);
 router.get('/logout', accountController.logout);  
 
 module.exports = router;
